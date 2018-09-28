64c79c1d1fc562b608cac282327eb3bd2ff40983
Phil <tarver.phillipe@gmail.com>
2018-09-28 13:46:04 -0500

    Add project files.

---
+++ BestBuyInClassProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestBuyInClassProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
